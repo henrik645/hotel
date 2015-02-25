67b3c1308b25cd8ae60fc42d4ec69f9f1e51417a
Henrik Vester <henrik645@gmail.com>
2015-02-25 15:44:53 +0100

    Added main logic + curses initialization

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
+import random, curses
 random.seed()
 
+VERSION = "0.1"
 GUESTS = {
     "John": "male",
     "Maria": "female",
@@ -9,9 +10,9 @@ GUESTS = {
     "Starly": "female",
     "Anna": "female"
 }
-FAILURE_RATES = {"normal": 0.1, "sturdy": 0.05}
+FAILURE_RATE = 0.1
 
-class Capsule():
+class Room():
     def __init__(self, type, guest=None, clean=True, broken=False):
         self.type = type
         self.failure_rate = FAILURE_RATES[type]
@@ -37,3 +38,32 @@ def remove_guest(room):
         room.clean = False
         if random.random() < room.failure_rate:
             room.broken = True
+
+def init():
+    print("Hotel Simulator " + VERSION)
+    print("Welcome!")
+    print("")
+    hotel_name = input("Hotel name: ")
+    print("")
+    print("You are a hotel owner. Your goal is to make as much money as possible without going bankrupt.")
+    
+def day(number):
+    for i in range(30):
+        print("")
+    
+    print("Day " + str(number))
+    time.sleep(2)
+    for i in range(30):
+        print(hh)
+
+screen = curses.initscr()
+curses.noecho()
+curses.cbreak()
+screen.keypad(1)
+
+screen.addstr(0, 0, "Test", curses.A_REVERSE)
+screen.refresh()
+
+curses.nocbreak()
+curses.echo()
+screen.keypad(0)
